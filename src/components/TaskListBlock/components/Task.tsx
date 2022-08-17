920b776868b1ed7cf103f1feecd6f49391f150ae
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-08-17 10:59:17 +0800

    faet: ui of TaskListBlock

--- src/components/TaskListBlock/components/Task.tsx
+++ src/components/TaskListBlock/components/Task.tsx
@@ -8,7 +8,9 @@ interface TaskProp {
 const Task: FC<TaskProp> = ({ title, content }) => {
 	return (
 		<div className='task'>
-			<h3 className='task-title'>{title}</h3>
+			<h3 className='task-title' title={title}>
+				{title}
+			</h3>
 			<p className='content'>{content}</p>
 		</div>
 	);
