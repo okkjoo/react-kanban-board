213c475709dcbc99cb02538e23aa778baffa4804
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-08-19 12:03:18 +0800

    feat: StartPage

--- src/components/StartPage/index.tsx
+++ src/components/StartPage/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import './StartPage.scss';
+
 const StartPage = () => {
-	return <div className='='>一个开始页面</div>;
+	return (
+		<div className='start-page'>
+			<h1 className='title'>Welcome to react-kanban-board✨</h1>
+			<div className='circle-list'>
+				<div className='circle'>
+					<div className='paragraph'>
+						<i>💦</i> Add your kanban with a plus sign in the bottom left
+					</div>
+				</div>
+				<div className='circle'>
+					<div className='paragraph'>
+						<i>⚡</i>shortcut key:
+						<br /> - D: add a kanban
+						<br />- E: add a 'to do' task
+					</div>
+				</div>
+				<div className='circle'>
+					<div className='paragraph'>
+						<i>💫</i> Features:
+						<br />- use LocalStorage
+						<br />- Draggable
+						<br />
+						<br />
+						<i>💥</i> Know more:
+						<br />
+						<a
+							href='https://github.com/okkjoo/react-kanban-board'
+							target='_blank'
+							rel='noreferrer'
+						>
+							github
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default StartPage;
